8265bb0795e7e0fe66ddd7bc08faaac0fda48044
Manpreet kaur <sekhonmani32@gmail.com>
2025-11-20 21:56:14 -0500

    load ToDo items from local storage and update on add

--- script.js
+++ script.js
@@ -4,7 +4,41 @@ let inputFieldElement = document.getElementById("inputField");
 let checkboxFieldElement = document.getElementById("checkboxField");
 let errormessageElement = document.getElementById("errorMessage");
 let todoContainerElement = document.getElementById("toDoContainer");
-let toDos = [];
+let inputfromlocalStroage = localStorage.getItem("toDosArray");
+let inputfromlocalStroageButParsed = JSON.parse(inputfromlocalStroage);
+let toDos = inputfromlocalStroageButParsed || [];
+let toDoshtml = "";
+
+console.log(inputfromlocalStroage, inputfromlocalStroageButParsed);
+inputfromlocalStroageButParsed.forEach((toDo, index) => {
+  toDoshtml += `<div
+        class="flex items-center justify-between bg-gray-100 w-[600px] mt-4 px-4 py-2 mx-auto"
+      >
+        <div>
+          <input id="checkboxField" type="checkbox" />
+                 
+          <span class= "${
+            toDo.isCompleted ? "checked bg-green-200 text-green-700" : ""
+          }">
+           ${toDo.title}</span>
+
+        </div>
+        <div>
+          <button id="editbtn" class="bg-purple-800 text-white rounded px-4 py-1">
+            Edit
+          </button>
+          <button
+            id="deletebtn"
+            class="bg-red-800 text-white rounded px-4 py-1"
+          >
+            Delete
+          </button>
+        </div> 
+      </div>`;
+});
+console.log(toDoshtml);
+todoContainerElement.innerHTML = toDoshtml;
+
 addbtnElement.addEventListener("click", () => {
   let toDoshtml = "";
   console.log(inputFieldElement.value);
@@ -15,6 +49,7 @@ addbtnElement.addEventListener("click", () => {
     title: userInput,
     isCompleted: false,
   };
+
   if (userInput == "") {
     inputFieldElement.style.border = "2px solid red";
     errormessageElement.style.display = "block";
@@ -26,9 +61,12 @@ addbtnElement.addEventListener("click", () => {
   inputFieldElement.style.border = "1px solid ";
   errormessageElement.style.display = "none";
 
+  let userString = JSON.stringify(toDos);
+  localStorage.setItem("toDosArray", userString);
+
   console.log(toDos);
   inputFieldElement.value = "";
-  
+
   toDos.forEach((toDo, index) => {
     toDoshtml += `<div
         class="flex items-center justify-between bg-gray-100 w-[600px] mt-4 px-4 py-2 mx-auto"
